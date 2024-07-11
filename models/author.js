c9f6920e60947fc951ea70518821c0c62e2fd046
tomason182 <elotro182@hotmail.com>
2024-07-11 16:19:44 -0300

    add author, book, genre, publisher models

---
+++ models/author.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
