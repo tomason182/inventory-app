2d978676c5941cbad00e6b4624842521ab600873
tomason182 <elotro182@hotmail.com>
2024-07-11 19:31:29 -0300

    add: mock publishers data for populate db

---
+++ publishers-data.js
@@ -0,0 +1,33 @@
+const publishersList = [
+  {
+    name: "Editorial Planeta",
+    address: "ing. Enrique Butty 275 Piso 8",
+    phoneNumber: "4124-9100",
+    email: "",
+  },
+  {
+    name: "Ediciones Lea",
+    description: "",
+    address: "Av. Dorrego 330, Villa Crespo",
+    phoneNumber: "4856-9056",
+    email: "info@edicioneslea.com",
+  },
+  {
+    name: "Penguin Libros",
+    address: "Humberto Primo 555",
+    phoneNumber: "5235-4400",
+    email: "recepcion@penguinrandomhouse.com",
+  },
+  {
+    name: "Albatros",
+    address: "Torre Las Plazas 2745 - Buenos Aires",
+    phoneNumber: "4807-1034",
+    email: "info@albatros.com",
+  },
+  {
+    name: "Alfaguara",
+    address: "Av. San Juan 2456",
+    phoneNumber: "2135-7824",
+    email: "info@alfagura.com",
+  },
+];
