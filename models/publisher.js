6c2fed61d98be9099fa41f30b891068d5b971a13
tomason182 <elotro182@hotmail.com>
2024-07-11 17:26:23 -0300

    fix semicolon issue

--- models/publisher.js
+++ models/publisher.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const PublisherSchema = new Schema({
-  company_name: { type: String, require: true, maxLength: 100},
-  address: {type: String, require: true, maxLength: 50};
-})
+  company_name: { type: String, require: true, maxLength: 100 },
+  address: { type: String, require: true, maxLength: 50 },
+});
 
-PublisherSchema.virtual("url").get(function() {
-  return `catalog/publisher/${this._id}`
-})
+PublisherSchema.virtual("url").get(function () {
+  return `catalog/publisher/${this._id}`;
+});
 
-module.exports = mongoose.model("Publisher", PublisherSchema)
+module.exports = mongoose.model("Publisher", PublisherSchema);
