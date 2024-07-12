94c889921b865079872e756ea4dabf11cda13031
tomason182 <elotro182@hotmail.com>
2024-07-11 22:16:47 -0300

    add bio and natinality to author schema

--- models/author.js
+++ models/author.js
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
+  bio: { type: String },
+  nationality: { type: String, required: true },
   date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
